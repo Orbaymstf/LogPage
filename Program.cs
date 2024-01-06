de324aa2d90286030892926664add07bab63d4a1
Mustafa Orbay <orbaymstf@gmail.com>
2024-01-06 03:47:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using LoginPage.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("LoginPageContextConnection") ?? throw new InvalidOperationException("Connection string 'LoginPageContextConnection' not found.");
+
+builder.Services.AddDbContext<LoginPageContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<LoginPageUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LoginPageContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
